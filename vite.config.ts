7f26766faa1e37eb3cbd3c2e0e20adb64628333b
Tony CABAYE <tony.cabaye@smile.fr>
2023-03-06 16:05:05 +0100

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+import ServerRef from 'vite-plugin-vue-server-ref'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    ServerRef({
+      debug: true,
+      state: {
+        nav: {
+          cursor: {
+            x: 0,
+            y: 0,
+          }
+        }
+      }
+    })
+  ]
+})
